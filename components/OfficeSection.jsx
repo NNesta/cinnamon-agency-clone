5ca435d290235b89c403566a48edf6961aa08941
Ngabonziza Nestor <ngabonest@gmail.com>
2022-11-04 14:36:41 +0200

    feat: reducing the width

--- components/OfficeSection.jsx
+++ components/OfficeSection.jsx
@@ -23,10 +23,10 @@ function OfficeSection() {
   const [scroll, setScroll] = useState(false);
   const clickHandler=()=>setScroll(!scroll)
   return (
-    <div className="w-full pl-32 py-32">
+    <div className="w-full lg:pl-32 py-32">
         <div className="flex flex-col gap-16 ">
           <h1 className="text-6xl font-mont-bold">Our offices</h1>
-      <div className={`mx-auto w-[1536px] relative duration-300 ease-in-out ${scroll?'-translate-x-[40%]':'translate-x-[0%]'}`}>
+      <div className={`mx-auto lg:w-[1536px] relative duration-300 ease-in-out ${scroll?'-translate-x-[40%]':'translate-x-[0%]'} overflow-scroll scrollbar-hide lg:overflow-visible`}>
           <div className="inline-flex gap-4">
             {/* <div className='absolute top-0 left-0 flex w-[400%] gap-8 snap-none'> */}
 
